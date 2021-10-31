03c0a1575691f61a4b2e7533015951b815d7315b
Douglas Almeida <douglas.rivan@zup.com.br>
2021-10-31 08:20:44 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { GlobalStyles } from "./styles/global";
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <span>Template React With JavaScript</span>
+    </>
+  );
+};
+
+export default App;
